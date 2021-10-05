0cbee86e59de52bf9a2f5220a90cf28ace75fc99
VAdam02 <61538113+VAdam02@users.noreply.github.com>
2021-10-05 14:47:02 +0200

    Joystick tested

--- ThermoSnake.ino
+++ ThermoSnake.ino
@@ -1,12 +1,42 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+
+#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
 int time = 0;
 
 void setup()
 {
-  
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  display.clearDisplay();
 }
 
 void loop()
 {
+  display.clearDisplay();
+  display.setTextSize(2);      // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE); // Draw white text
+  display.setCursor(0, 0);     // Start at top-left corner
+  display.cp437(true);
+  
+  String asd = "VRX ";
+  asd += String(analogRead(A7));
+  asd += "\nVRY ";
+  asd += String(analogRead(A6));
+  display.print(asd);
+  display.display();
+  
   delay(100);
   time++;
 }
