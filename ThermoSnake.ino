c317033e0932c9a5a7ff6edd20e6beca1ecf0485
VAdam02 <61538113+VAdam02@users.noreply.github.com>
2021-10-12 10:37:33 +0200

    Equal delay between repeats

--- ThermoSnake.ino
+++ ThermoSnake.ino
@@ -3,11 +3,27 @@ void setup()
   
 }
 
+int Delay_lastTime = 0;
 void loop()
 {
-  unsigned long startTime = millis();
-
   //Code goes here
   
-  delay(50-(unsigned long)(millis() - startTime));
+  //calculate deltatime
+  int deltatime = (int)(millis() % 32768);
+  if (deltatime <= Delay_lastTime)
+  {
+      Delay_lastTime = 0 - (32767 - Delay_lastTime);
+  }
+  deltatime = deltatime - Delay_lastTime;
+  //calculate deltatime
+
+  if (deltatime < 50)
+  {
+    delay(50-deltatime);
+  }
+  else
+  {
+    //FIXME probably something go really wrong with time consume so manage this
+  }
+  Delay_lastTime += 50;
 }
