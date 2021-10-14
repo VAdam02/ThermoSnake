7b01894d769cdcb277c60a5067c389cc63757aa9
VAdam02 <61538113+VAdam02@users.noreply.github.com>
2021-10-14 11:42:48 +0200

    Better deltatime calculator algorithm

--- src/DelayManager/DelayManager.cpp
+++ src/DelayManager/DelayManager.cpp
@@ -3,30 +3,24 @@
 
 DelayManager::DelayManager() { }
 
-int Delay_lastTime = 0;
+unsigned int Delay_lastTime = 0;
 void DelayManager::begin()
 {
-    Delay_lastTime = millis();
+  Delay_lastTime = (unsigned int)(millis() % 65536);
 }
 
 void DelayManager::sleepReamingOf(int timeOfExecute)
 {
-    //calculate deltatime
-    int deltatime = (int)(millis() % 32768);
-    if (deltatime <= Delay_lastTime)
-    {
-        Delay_lastTime = 0 - (32767 - Delay_lastTime);
-    }
-    deltatime = deltatime - Delay_lastTime;
-    //calculate deltatime
+  unsigned int deltatime = (unsigned int)(millis() % 65536) - Delay_lastTime;
 
-    if (deltatime < timeOfExecute)
-    {
-        delay(timeOfExecute-deltatime);
-    }
-    else
-    {
-        //FIXME probably something go really wrong with time consume so manage this
-    }
-    Delay_lastTime += timeOfExecute;
+  if (deltatime < timeOfExecute)
+  {
+    delay(timeOfExecute-deltatime);
+  }
+  else
+  {
+    //FIXME probably something go really wrong with time consume so manage this
+  }
+
+  Delay_lastTime += timeOfExecute;
 }
